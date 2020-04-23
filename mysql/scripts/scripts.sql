a0a0a2861c76149f051cc6640732395456766f2c
Roger Santos <rogerioantoniodossantos@outlook.com>
2020-04-22 21:07:43 -0300

    Estrutura base do mysql para testes

---
+++ mysql/scripts/scripts.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS app;
+
+use app;
+
+CREATE TABLE IF NOT EXISTS post(
+    id INT(11) AUTO_INCREMENT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    FULLTEXT(title, content),
+    PRIMARY KEY (id)
+);
